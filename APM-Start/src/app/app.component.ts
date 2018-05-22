6f7bf6ab3eaf173e467796383da3cb3d9bf73f55
ray <rsmith52@wisc.edu>
2018-05-22 14:55:21 -0500

    Created new component for product list, edited app component's html to include it, and declared the new component in app.module

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'pm-root', // product management root Component
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+      <pm-products></pm-products>
     </div>
     `
 })
